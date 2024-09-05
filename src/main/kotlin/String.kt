4543ff4b0f419c324808d1800e960789448ccce4
dewiafrianiii <afrianidewi551@gmail.com>
2024-09-05 14:56:20 +0700

    Push Stater Project.

---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val myName: String = "Dewi Afriani"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+    val s = "abc" + 1
+    println(s + "def")
+}
